139cda9ac3ec2adc8c09153507cf842013d92827
showter <shota.nskc@gmail.com>
2022-04-08 06:50:03 +0900

    Ruby 課題（基礎）

--- task.rb
+++ task.rb
@@ -121,7 +121,7 @@ def q12
 
   # 以下に回答を記載
   puts data.dig(:user, :name)
-  
+
 end
 
 def q13
@@ -129,21 +129,37 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  user_data.merge!(update_data)
+  p user_data
 end
 
 def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+  data_keys = data.keys
+    p data_keys
 end
 
 def q15
   data1 = { name: "saitou", hobby: "soccer", age: 33, role: "admin" }
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
-  # 以下に回答を記載
+    # 以下に回答を記載
+
+  if data1.key?(:age)
+    p "OK"
+
+  else
+    p "NG"
+  end
+
+  if data2.key?(:age)
+    p "OK"
+
+  else
+    p "NG"
+  end
 
 end
 
@@ -156,7 +172,11 @@ def q16
   ]
 
   # 以下に回答を記載
-
+ 
+  
+ users.each do |user|
+   puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です"
+  end
 end
 
 class UserQ17
